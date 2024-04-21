4ea09147b12ff5709adaf93807eb58c6f602efa2
Apollo-Hb <163935347+Apollo-Hb@users.noreply.github.com>
2024-04-21 10:26:32 -0300

    Update atividade 24.py

--- atividade 24.py
+++ atividade 24.py
@@ -1,5 +1,5 @@
 ###
-# atividade 15
+# atividade 24
 # aluno: Apollo
 # Data: 08/04/2024
 ##A tarifas de certo parque de estacionamento são as seguintes: a. 1ª e 2ª hora – R$ 1,00 cada b. 3ª e 4ª hora – R$ 1,40 cada c. 5ª hora e seguintes R$ 2,00 cada. O numero de horas a pagar é sempre inteiro arredondado por excesso. Deste modo, quem estacionar durante 61 minutos pagará por duas horas, que é o mesmo que pagaria se tivesse permanecido 120 minutos. Os momentos de chegada ao parque e partida são apresentados na foram de pares de inteiros, representando horas e minutos. Por exemplo, o par 12 50 representará “dez para a uma da tarde”. Pretendense criar um programa que, lidos pelo teclado s momento de chegada e de partida, escreva na tela o preço cobrado pelo estacionamento. Admite-se que a chega e a partida se dão com intervalor não superior a 24 horas. Portanto, se uma dada hora de chegada for superior à partida, isso não é uma situação de erro, antes significará que a partida ocorreu no dia seguinte ao da chegada.
